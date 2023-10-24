f941737a06a73fbc3618348e4680ec080802c1a4
yahya9821 <mohamedyahya.jday@esprit.tn>
2023-10-24 21:27:53 +0200

    test unitaire with mockito

--- src/test/java/tn/esprit/devops_project/services/SupplierServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/SupplierServiceImplTest.java
@@ -1,103 +1,95 @@
 package tn.esprit.devops_project.services;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 import tn.esprit.devops_project.entities.Supplier;
-import tn.esprit.devops_project.entities.SupplierCategory;
 import tn.esprit.devops_project.repositories.SupplierRepository;
-import tn.esprit.devops_project.services.Iservices.ISupplierService;
 
-import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-// im just testing the webhook , please work
 @SpringBootTest
-@Transactional
 public class SupplierServiceImplTest {
 
-    @Autowired
-    private ISupplierService supplierService;
+    @InjectMocks
+    SupplierServiceImpl supplierService;
 
-    @Autowired
-    private SupplierRepository supplierRepository;
-
-    @BeforeEach
-    public void setUp() {
-        // You can initialize test data or perform any setup if needed.
-    }
-
-    @Test
-    public void testRetrieveAllSuppliers() {
-        List<Supplier> suppliers = supplierService.retrieveAllSuppliers();
-        assertEquals(0, suppliers.size()); // Assuming no data is inserted initially.
-    }
+    @Mock
+    SupplierRepository supplierRepository;
 
     @Test
     public void testAddSupplier() {
-        Supplier supplier = new Supplier();
-        supplier.setLabel("NewSupplier");
-        supplier.setCode("NS001");
-        supplier.setSupplierCategory(SupplierCategory.ORDINAIRE); // Replace with the actual category enum value
+        // Create a Supplier instance and set its properties
+        Supplier addedSupplier = new Supplier();
+        addedSupplier.setLabel("Supplier Label");
 
-        Supplier addedSupplier = supplierService.addSupplier(supplier);
+        // Mock the behavior of your repository to return the addedSupplier when save is called
+        Mockito.when(supplierRepository.save(Mockito.any(Supplier.class))).thenReturn(addedSupplier);
 
-        Optional<Supplier> retrievedSupplier = supplierRepository.findById(addedSupplier.getIdSupplier());
+        // Call the service method you want to test
+        Supplier savedSupplier = supplierService.addSupplier(addedSupplier);
 
-        assertEquals("NewSupplier", retrievedSupplier.get().getLabel());
+        // Assertions
+        assertNotNull(savedSupplier);
+        // Add more specific assertions based on the behavior you expect
     }
-
+/*
     @Test
     public void testUpdateSupplier() {
-        Supplier supplier = new Supplier();
-        supplier.setLabel("OldSupplier");
-        supplier.setCode("OS001");
-        supplier.setSupplierCategory(SupplierCategory.ORDINAIRE);
+        // Create a Supplier instance and set its properties
+        Supplier addedSupplier = new Supplier();
+        addedSupplier.setIdSupplier(1L);
+        addedSupplier.setLabel("Supplier Label");
 
-        Supplier addedSupplier = supplierService.addSupplier(supplier);
+        // Mock any required dependencies or services
+        // For example, you can mock the behavior of your repository to return the addedSupplier
 
-        addedSupplier.setLabel("UpdatedSupplier");
+        // Call the service method you want to test
         Supplier updatedSupplier = supplierService.updateSupplier(addedSupplier);
 
-        assertEquals("UpdatedSupplier", updatedSupplier.getLabel());
+        // Assertions
+        assertNotNull(updatedSupplier);
+        // Add more specific assertions based on the behavior you expect
+    }
+
+*/
+
+    @Test
+    public void testRetrieveSupplier() {
+        // Create a Supplier instance and set its properties
+        Supplier addedSupplier = new Supplier();
+        addedSupplier.setIdSupplier(1L);
+        addedSupplier.setLabel("Supplier Label");
+
+        // Mock the behavior of your repository to return a Supplier when findById is called
+        Mockito.when(supplierRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(addedSupplier));
+
+        // Call the service method you want to test
+        Supplier retrievedSupplier = supplierService.retrieveSupplier(1L);
+
+        // Assertions
+        assertNotNull(retrievedSupplier);
+        // Add more specific assertions based on the behavior you expect
     }
 
     @Test
     public void testDeleteSupplier() {
-        Supplier supplier = new Supplier();
-        supplier.setLabel("SupplierToDelete");
-        supplier.setCode("SD001");
-        supplier.setSupplierCategory(SupplierCategory.ORDINAIRE);
+        // Create a Supplier instance and set its properties
+        Supplier addedSupplier = new Supplier();
+        addedSupplier.setIdSupplier(1L);
+        addedSupplier.setLabel("Supplier Label");
 
-        Supplier addedSupplier = supplierService.addSupplier(supplier);
+        // Mock the behavior of your repository to return a Supplier when findById is called
+        Mockito.when(supplierRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(addedSupplier));
 
-        supplierService.deleteSupplier(addedSupplier.getIdSupplier());
+        // Call the service method you want to test
+        supplierService.deleteSupplier(1L);
 
-        Optional<Supplier> deletedSupplier = supplierRepository.findById(addedSupplier.getIdSupplier());
-        assertEquals(Optional.empty(), deletedSupplier);
-    }
-
-    @Test
-    public void testRetrieveSupplier() {
-        Supplier supplier = new Supplier();
-        supplier.setLabel("TestSupplier");
-        supplier.setCode("TS001");
-        supplier.setSupplierCategory(SupplierCategory.ORDINAIRE);
-
-        Supplier addedSupplier = supplierService.addSupplier(supplier);
-
-        Supplier retrievedSupplier = supplierService.retrieveSupplier(addedSupplier.getIdSupplier());
-
-        assertEquals("TestSupplier", retrievedSupplier.getLabel());
-    }
-
-    @Test
-    public void testRetrieveSupplier_InvalidId() {
-        assertThrows(IllegalArgumentException.class, () -> supplierService.retrieveSupplier(999L));
+        // Assertions
+        // Verify that the delete method was called or add more specific assertions
     }
 }
