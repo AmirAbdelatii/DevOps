afbaaaf3fa80f1dde17508b7949726139c05e50f
aziz-kck <61433292+aziz-kck@users.noreply.github.com>
2023-11-07 23:25:38 +0100

    mockito+pom

--- src/main/java/tn/esprit/devops_project/services/StockServiceImplTest.java
+++ src/main/java/tn/esprit/devops_project/services/StockServiceImplTest.java
@@ -1,59 +1,47 @@
 package tn.esprit.devops_project.services;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tn.esprit.devops_project.entities.Stock;
 import tn.esprit.devops_project.repositories.StockRepository;
-import tn.esprit.devops_project.services.StockServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 class StockServiceImplTest {
 
-    public Stock stock;
     @Mock
-    StockRepository stockRepository= mock(StockRepository.class);
+    private StockRepository stockRepository;
 
     @InjectMocks
-    public StockServiceImpl stockServiceImpl;
-
-    Stock stock1 = new Stock(99L, "S");
-
-    List<Stock> stocklist = new ArrayList<Stock>() {
-
-        {
-            add(new Stock(1L, "S1"));
-            add(new Stock(2L, "S2"));
-            add(new Stock(3L, "S3"));
-
-        }};
-
+    private StockServiceImpl stockService;
 
     @Test
-    void retrieveAllStocktest() {
-        when(stockServiceImpl.retrieveAllStock()).thenReturn(stocklist);
-        List<Stock> stockList1 = stockServiceImpl.retrieveAllStock();
-        Assertions.assertEquals(3, stockList1.size());
-        System.out.println("***** retrieveAllStocktest Mockito : success *****");
+    void retrieveAllStockTest() {
+        List<Stock> expectedStockList = new ArrayList<>();
+        expectedStockList.add(new Stock(1L, "S1"));
+        expectedStockList.add(new Stock(2L, "S2"));
+        expectedStockList.add(new Stock(3L, "S3"));
+
+        when(stockRepository.findAll()).thenReturn(expectedStockList);
+        List<Stock> resultStockList = stockRepository.findAll();
+        assertEquals(3, resultStockList.size(), "Size mismatch");
+
     }
 
     @Test
-    public void testRetrieveStock_WhenStockExists() {
+    public void testRetrieveStockWhenStockExists() {
         Stock stock1 = new Stock(99L, "S");
 
         StockRepository stockRepository = mock(StockRepository.class);
